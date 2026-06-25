61ed5b74b0a208cabfea6939893aed2ec29cbc50
md-zeon <zeon.cse@gmail.com>
2026-06-25 14:55:52 +0600

    feat(auth): add cookie-parser middleware and implement checkAuth middleware for session and access token verification; secure specialty routes

--- src/app/module/specialty/specialty.route.ts
+++ src/app/module/specialty/specialty.route.ts
@@ -1,11 +1,26 @@
 import { Router } from "express";
 import { SpecialtyController } from "./specialty.controller";
+import checkAuth from "../../middleware/checkAuth";
+import { Role } from "../../../generated/prisma/enums";
 
 const router: Router = Router();
 
-router.post("/", SpecialtyController.createSpecialty);
+router.post(
+  "/",
+  checkAuth(Role.SUPER_ADMIN, Role.ADMIN),
+  SpecialtyController.createSpecialty,
+);
 router.get("/", SpecialtyController.getAllSpecialties);
-router.patch("/:id", SpecialtyController.updateSpecialty);
-router.delete("/:id", SpecialtyController.deleteSpecialty);
+
+router.patch(
+  "/:id",
+  checkAuth(Role.SUPER_ADMIN, Role.ADMIN),
+  SpecialtyController.updateSpecialty,
+);
+router.delete(
+  "/:id",
+  checkAuth(Role.SUPER_ADMIN, Role.ADMIN),
+  SpecialtyController.deleteSpecialty,
+);
 
 export const SpecialtyRoutes = router;
